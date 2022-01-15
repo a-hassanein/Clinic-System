27efd3d81d8ea865e8f1bcf4bf6be15b7b255937
a-hassanein <ahassaneina7@gmail.com>
2022-01-15 20:56:36 +0200

    standard color

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router , Route , Routes } from "react-router-dom";
 import NavBar from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
           <>
