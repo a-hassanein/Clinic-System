6a214b73a82028bbc42ee220e2c2bec6de44e8e0
abrar4799 <abrar.it99@gmail.com>
2022-01-19 13:33:43 +0200

    package edit

--- src/Components/CardPackages.js
+++ src/Components/CardPackages.js
@@ -6,27 +6,27 @@ import Packages from "./Packages";
 function CardPackages(props) {
   return (
     <div>
-      <Card style={{ width: "20rem" , height: "25rem"}} className="Card__bg ">
-        <Card.Body>
-          <Card.Title className="Card__Title">{props.title} <br/> </Card.Title> 
+      <div  style={{ width: "20rem" , height: "25rem"}} className="card Card__body">
+        
+          <div className="Card__Title">{props.title} <br/> </div> 
          
-          <Card.Subtitle className="mb-2 Card__Subtitle">
+          <div className="Card__Subtitle">
            <span>{props.Subtitle}</span><span className="span__Egy">{props.coin}</span> <span className="span__Per">{props.per}</span>
-          </Card.Subtitle>
+          </div>
          
-          <Card.Text>
+         
             <ul className="Card__list">
                 <li>{props.list0} </li>
                 <li>{props.list1} </li>
                 <li>{props.list2}</li>
                
             </ul>
-            </Card.Text>
+         
          <Button className="Card__Button">Select</Button>
         
           
-        </Card.Body>
-      </Card>
+       
+      </div>
     </div>
   );
 }
