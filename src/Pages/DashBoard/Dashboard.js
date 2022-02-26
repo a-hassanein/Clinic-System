92e0f7e25b907c298eb4338d0950579a1856c027
ALaa Farouk <alaafar19@gmail.com>
2022-02-26 06:45:27 +0200

    patient but not completed

--- src/Pages/DashBoard/Dashboard.js
+++ src/Pages/DashBoard/Dashboard.js
@@ -3,6 +3,7 @@ import Sidebar from '../DashBoard/Sidebar'
 import SignUp from '../signup';
 import Home from '../../Components/Home';
 import Surgery from '../DashBoard/surgery';
+import CreatePatient from '../DashBoard/createpatient';
 import Bill from '../DashBoard/Bill';
 import Prescripe from './Prescription'
 import Patient from './Patient'
@@ -24,7 +25,6 @@ const Dashboard = () => {
                     <Route path="/bill" component={Bill} />
                     <Route path="/prescription" component={Prescripe} />
                     <Route path="/patient" component={Patient} />
-
                     <Route path="/materials" component={Materials}/>
                     <Route path="/addmaterial" component={Addmaterial}/>
                 </Switch>
