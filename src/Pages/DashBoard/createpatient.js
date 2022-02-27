823beb2f2875dec918a8a68f2b695329a4ec00f4
ALaa Farouk <alaafar19@gmail.com>
2022-02-27 06:04:28 +0200

    patient but not responsive

--- src/Pages/DashBoard/createpatient.js
+++ src/Pages/DashBoard/createpatient.js
@@ -11,7 +11,10 @@ function CreatePatient() {
 
         <>
             <Sidebar />
-            <section className='home-section' >
+            <section className='home-section' style={{ marginTop: '20px', }} >
+                <div>
+                    <h2 className="header_patient"> ADD NEW PATIENT </h2>
+                </div>
                 <div className="container" id="navbar_container" >
                     <nav className="navbar navbar-expand-lg ">
                         <div className="container-fluid" >
@@ -33,7 +36,7 @@ function CreatePatient() {
                                     </li>
                                 </ul>
                                 <form className="d-flex">
-                                    <input className="form-control me-2" type="search" placeholder="Search For Patient" aria-label="Search" id="searchfield" />
+                                    <input className="form-control me-2" type="search" placeholder="Search For Patient" aria-label="Search" id="searchfield" required />
                                     <button className="btn" id="searchbtn" type="submit">Search</button>
                                 </form>
                             </div>
@@ -42,68 +45,105 @@ function CreatePatient() {
                 </div>
                 <div className="container" id="createpatientForm">
                     <div className="container-fluid">
-                        <div>
-                            <h2 id="header_form"> Welcome , Add New Patient</h2>
-                        </div>
                         <form>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">First Name</label>
-                                <div className="col-sm-10">
-                                    <input type="text" className="form-control" />
+                            <div class="row align-items-center">
+                                <h4 id="personal_information">Personal Information</h4>
+                                <div class="col-md-6">
+                                    <label class="col-form-label">FIRST NAME</label>
+                                    <input class="form-control" type="text" required />
+                                    {/* <div class="valid-feedback">Good! Patient First Name looks valid.</div>
+                                    <div class="invalid-feedback">Opps! You have entered an invalid password.</div> */}
+
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Last Name</label>
-                                <div className="col-sm-10">
-                                    <input type="text" className="form-control" />
+                                <div class="col-md-6">
+                                    <label class="col-form-label">LAST NAME</label>
+                                    <input class="form-control" type="text" required />
+                                    {/* <div class="valid-feedback">Good! Patient Last Name looks valid.</div> */}
                                 </div>
+
                             </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Address</label>
-                                <div className="col-sm-10">
-                                    <input type="text" className="form-control" />
+                            <div class="row g-3 align-items-center">
+                                <div class="col-md-8">
+                                    <label class="col-form-label">ADDRESS</label>
+                                    <input class="form-control" type="text" required />
+
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Phone Number</label>
-                                <div className="col-sm-10">
-                                    <input type="phone" className="form-control" />
+                                <div class="col-md-4">
+                                    <label class="col-form-label">PHONE NUMBER</label>
+                                    <input class="form-control" type="phonenumber" required />
+
                                 </div>
+
                             </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Phone Number</label>
-                                <div className="col-sm-10">
-                                    <input type="phone" className="form-control" />
+                            <div class="row g-3 align-items-center">
+                                <div class="col-md-6">
+                                    <label class="col-form-label">EMAIL</label>
+                                    <input class="form-control" type="email" required />
+
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Phone Number</label>
-                                <div className="col-sm-10">
-                                    <input type="phone" className="form-control" />
+                                <div class="col-md-3">
+                                    <label class="col-form-label">AGE</label>
+                                    <input class="form-control" type="number" required />
+
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Phone Number</label>
-                                <div className="col-sm-10">
-                                    <input type="phone" className="form-control" />
+                                <div class="col-md-3">
+                                    <label class="col-form-label">GENDER</label>
+                                    <select class="form-select">
+                                        <option>Female</option>
+                                        <option>Male</option>
+
+                                    </select>
                                 </div>
+
                             </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Phone Number</label>
-                                <div className="col-sm-10">
-                                    <input type="phone" className="form-control" />
+                            <div class="row align-items-center">
+                                <h4 id="medical_details">MEDICAL DETAILS</h4>
+                                <div class="col-md-6">
+                                    <label class="col-form-label">DOCTOR NAME</label>
+                                    <input class="form-control" type="text" required />
+
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label className="col-sm-2 col-form-label">Phone Number</label>
-                                <div className="col-sm-10">
-                                    <input type="phone" className="form-control" />
+                                <div class="col-md-6">
+                                    <label class="col-form-label">SURGERY NAME</label>
+                                    <input class="form-control" type="text" required />
+
                                 </div>
+
                             </div>
+                            <div class="row align-items-center">
+                                <div class="col-md-4">
+                                    <label class="col-form-label">Scanning</label>
+                                    <input class="form-control" type="text" required />
+
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="col-form-label">CURRENT DISEASE</label>
+                                    <input class="form-control" type="text" required />
+
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="col-form-label">QUANTITY OF DRUG</label>
+                                    <input class="form-control" type="text" required />
+                                </div>
+
+                            </div>
+                            <div class="row align-items-center">
+                                <div class="col-md-6">
+                                    <label class="col-form-label">VISITA</label>
+                                    <input class="form-control" type="text" required />
+
+                                </div>
+                                <div class="col-md-6">
+                                    <label class="col-form-label">COMMENTS</label>
+                                    <input class="form-control" type="text" required />
+
+                                </div>
                             
+                            </div>
 
 
-                            <button type="submit" id="searchbtn" style={{marginLeft:"440px"}} className="btn">Add Patient</button>
+
+                            <button type="submit" id="searchbtn"  style={{ marginLeft: "520px" , marginTop:"20px" , width:"200px"}} className="btn align-items-center">Add Patient</button>
                         </form>
                     </div>
                 </div>
